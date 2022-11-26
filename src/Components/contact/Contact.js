9e45ac24e36d8db09e52212eb2f3370838aa4319
marc-antoine ourradour <marc-antoineourradour@MacBook-Air-de-marc-antoine.local>
2022-11-26 04:16:06 +0100

    contact backend +publish

--- src/Components/contact/Contact.js
+++ src/Components/contact/Contact.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   ContactContainer,
   ContactSection,
@@ -11,8 +11,29 @@ import {
   TextInput,
   ButtonSend,
 } from "./Contact.styled";
+import axios from "axios";
 
 const Contact = (props) => {
+  const [contact, setContact] = useState({
+    id: null,
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const query = (contact) => {
+    axios({
+      method: "GET",
+      url: "http://localhost:8080/",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      params: contact,
+    }).then((res) => console.log(res.data));
+  };
+  useEffect(() => {}, []);
+
   return (
     <ContactContainer>
       <ContactSection>
@@ -28,20 +49,52 @@ const Contact = (props) => {
           <ContainerInput>
             <ContainerHeader>
               <TextInput
+                onChange={(e) =>
+                  setContact({ ...contact, name: e.target.value })
+                }
+                value={contact.name}
                 width={"50%"}
                 placeholder="Insert your name"
               ></TextInput>
               <TextInput
+                onChange={(e) =>
+                  setContact({ ...contact, email: e.target.value })
+                }
+                value={contact.email}
                 width={"50%"}
                 placeholder="Insert your email"
               ></TextInput>
             </ContainerHeader>
-            <TextInput placeholder="Insert your Subject"></TextInput>
             <TextInput
+              onChange={(e) =>
+                setContact({ ...contact, subject: e.target.value })
+              }
+              value={contact.subject}
+              placeholder="Insert your Subject"
+            ></TextInput>
+            <TextInput
+              onChange={(e) =>
+                setContact({ ...contact, message: e.target.value })
+              }
+              value={contact.message}
               height={"10rem"}
               placeholder="Write a message"
             ></TextInput>
-            <ButtonSend>Send Message</ButtonSend>
+            <ButtonSend
+              onClick={() => {
+                console.log(contact);
+                query(contact);
+                setContact({
+                  id: null,
+                  name: "",
+                  email: "",
+                  subject: "",
+                  message: "",
+                });
+              }}
+            >
+              Send Message
+            </ButtonSend>
           </ContainerInput>
         </ContainerContent>
       </ContactSection>
